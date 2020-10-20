b4dfc408d2ef6a987d91aef724e110cce2c76507
alexjcwitsil <55110145+alexjcwitsil@users.noreply.github.com>
2020-10-20 08:23:53 -0600

    change repo from usgs to alexjcwitsil

--- hourssince.rb
+++ hourssince.rb
@@ -1,7 +1,9 @@
 class Hourssince < Formula
   desc "Functions and subroutines that calculate the number of hours of a given calendar date from Jan. 1 of a reference year"
-  homepage "https://github.com/usgs/volcano-ash3d-hourssince"
-  url "https://github.com/usgs/volcano-ash3d-hourssince/archive/master.tar.gz"
+  #homepage "https://github.com/usgs/volcano-ash3d-hourssince"
+  #url "https://github.com/usgs/volcano-ash3d-hourssince/archive/master.tar.gz"
+  homepage "https://github.com/alexjcwitsil/volcano-ash3d-hourssince"
+  url "https://github.com/alexjcwitsil/volcano-ash3d-hourssince/archive/master.tar.gz"
   version "0.2"  # I had to make up a version number since there are no releases
   sha256 "2a4b1fb6e872ac49e45ccef7758186c8b78c64e71e58cc8dd23dea610ef43793"
 
